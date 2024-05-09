8c2b52536a86b7f2f45d34fbbfa0c3d7067affbb
MaheshtheDev <mahesh.svmr@gmail.com>
2024-05-08 21:40:50 -0700

    Add PWA Support and metadata

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  //disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  // next.js config
+});
